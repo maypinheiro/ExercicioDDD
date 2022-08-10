e03e25e410126074eec2274c164a1cbf56b83e2f
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-10 08:47:20 -0300

    Exercicio ddd

---
+++ Ddd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDDD
+{
+    class Ddd
+    {
+
+
+        public string VerificaDDD(int ddd)
+        {
+                 if (ddd == 61) return ("Brasilia");
+            else if (ddd == 71)return("Salvador");
+            else if (ddd == 11) return("São Paulo");
+            else if (ddd == 21) return("Rio de Janeiro");
+            else if (ddd == 32) return("Juíz de Fora");
+            else if (ddd == 19) return("Campinas");
+            else if (ddd == 31) return("Belo Horizonte");
+            else return("DDD não cadastrado");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExercicioDDD
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Ddd verificadd = new Ddd();
+            int ddd = int.Parse(Console.ReadLine());
+            Console.WriteLine(verificadd.VerificaDDD(ddd));
+            Console.ReadKey();
+        }
+    }
+}
